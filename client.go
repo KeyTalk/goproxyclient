4c9ceba4d0e9d6f420baae9cd2b85b80220599dd
Remco <remco@dutchcoders.io>
2016-09-12 16:48:56 +0200

    working

--- client.go
+++ client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
@@ -14,7 +13,6 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"math/big"
 	"net"
 	"net/http"
@@ -43,15 +41,6 @@ import (
 
 // var log = logging.MustGetLogger("keytalk/client")
 
-var loggedIn = false
-
-type Transport struct {
-}
-
-func (t *Transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	return http.DefaultTransport.RoundTrip(req)
-}
-
 type Client struct {
 	listener          net.Listener
 	TLSListenerString string `toml:"tlslisten"`
@@ -66,7 +55,8 @@ type Client struct {
 		Level  string `toml:"level"`
 	} `toml:"logging"`
 
-	rccds []*rccd.RCCD
+	template *template.Template
+	rccds    []*rccd.RCCD
 }
 
 func New() *Client {
@@ -75,20 +65,6 @@ func New() *Client {
 	}
 }
 
-func (c *Client) HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("TEST"))
-}
-
-func orPanic(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-/*
-func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-}
-*/
 type pipeResponseWriter struct {
 	r     *io.PipeReader
 	w     *io.PipeWriter
@@ -119,11 +95,15 @@ func (w *pipeResponseWriter) WriteHeader(status int) {
 }
 
 type RoundTripper2 struct {
+	client *Client
 }
 
+// todo(nl5887): make context specific, this is specific to the provider
+// we're logging in per provider, not per service
 var (
-	cert99 *openssl.Certificate
-	pk99   openssl.PrivateKey
+	loggedIn = false
+	cert99   *openssl.Certificate
+	pk99     openssl.PrivateKey
 )
 
 func (rt *RoundTripper2) RoundTrip(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Response, error) {
@@ -177,6 +157,7 @@ func (rt *RoundTripper2) RoundTrip(req *http.Request, ctx *goproxy.ProxyCtx) (*h
 }
 
 type RoundTripper struct {
+	client   *Client
 	rccd     *rccd.RCCD
 	provider *rccd.Provider
 	service  *rccd.Service
@@ -200,78 +181,77 @@ func (rt *RoundTripper) RoundTrip(req *http.Request, ctx *goproxy.ProxyCtx) (*ht
 		defer w.Close()
 
 		var router = mux.NewRouter()
-		router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			// should redirect to internal url.
+		router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var (
 				username string
 				password string
 			)
 
-			invalid := false
+			message := ""
 
 			if r.Method == "POST" {
-				kc, err := keytalk.New(rt.rccd, fmt.Sprintf("https://%s", rt.provider.Server))
-				if err != nil {
-					// todo(nl5887): return body with error
-					panic(err)
-					log.Error("Error initializing Keytalk client: %s", err.Error())
-				}
-
-				username = r.PostFormValue("username")
-				password = r.PostFormValue("password")
-
-				if uc, err := kc.Authenticate(username, password, rt.service.Name); err != nil {
-					// todo(nl5887): return body with error
-					log.Error("Error authenticating with Keytalk: %s", err.Error())
-					panic(err)
-				} else {
-					fmt.Printf("Got user certificate: %#v\n", uc)
-					// got certificate, store certificate
-					cert2 := &pem.Block{Type: "CERTIFICATE", Bytes: uc.Raw}
-					certstr := pem.EncodeToMemory(cert2)
-
-					cert99, err = openssl.LoadCertificateFromPEM(certstr)
+				for {
+					kc, err := keytalk.New(rt.rccd, fmt.Sprintf("https://%s", rt.provider.Server))
 					if err != nil {
-						log.Error("Error creating openssl ctx: %s", err.Error())
-						panic(err)
+						// todo(nl5887): return body with error
+						message = fmt.Sprintf("Error authenticating with Keytalk: %s", err.Error())
+						break
 					}
 
-					key := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(uc.PrivateKey().(*rsa.PrivateKey))}
-					keystr := pem.EncodeToMemory(key)
+					username = r.PostFormValue("username")
+					password = r.PostFormValue("password")
 
-					pk99, err = openssl.LoadPrivateKeyFromPEM(keystr)
-					if err != nil {
-						log.Error("Error creating openssl ctx: %s", err.Error())
-						panic(err)
+					if uc, err := kc.Authenticate(username, password, rt.service.Name); err != nil {
+						// todo(nl5887): return body with error
+						log.Error("Error authenticating with Keytalk: %s", err.Error())
+						message = fmt.Sprintf("Error authenticating with Keytalk: %s", err.Error())
+						break
+					} else {
+						fmt.Printf("Got user certificate: %#v\n", uc)
+						// got certificate, store certificate
+						cert2 := &pem.Block{Type: "CERTIFICATE", Bytes: uc.Raw}
+						certstr := pem.EncodeToMemory(cert2)
+
+						cert99, err = openssl.LoadCertificateFromPEM(certstr)
+						if err != nil {
+							log.Error("Error creating openssl ctx: %s", err.Error())
+							panic(err)
+						}
+
+						key := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(uc.PrivateKey().(*rsa.PrivateKey))}
+						keystr := pem.EncodeToMemory(key)
+
+						pk99, err = openssl.LoadPrivateKeyFromPEM(keystr)
+						if err != nil {
+							log.Error("Error creating openssl ctx: %s", err.Error())
+							panic(err)
+						}
+
+						loggedIn = true
+						w.Header().Set("Connection", "close")
+
+						w.Header().Set("Location", r.URL.String())
+						w.WriteHeader(http.StatusFound)
+						return
 					}
 
-					loggedIn = true
-					w.Header().Set("Connection", "close")
-
-					w.Header().Set("Location", r.URL.String())
-					w.WriteHeader(http.StatusFound)
-					return
+					break
 				}
-
-				invalid = true
 			}
 
-			t, err := template.New("index.html").ParseFiles("./static/index.html")
-			if err != nil {
-				panic(err)
-			}
-
-			err = t.Execute(w, map[string]interface{}{
+			if err := rt.client.template.Execute(w, map[string]interface{}{
 				"username": username,
 				"password": password,
-				"invalid":  invalid,
-			})
-			if err != nil {
+				"message":  message,
+				"service":  rt.service,
+				"provider": rt.provider,
+			}); err != nil {
+				log.Error("Error executing template: %s", err.Error())
 				panic(err)
 			}
 		})
 
-		router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+		// router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 		router.ServeHTTP(prw, req)
 	}()
 	<-ready
@@ -447,7 +427,7 @@ func TLSConfigFromCA(ca *tls.Certificate) func(host string, ctx *goproxy.ProxyCt
 	}
 }
 
-func (c *Client) visit(path string, f os.FileInfo, err error) error {
+func (client *Client) visit(path string, f os.FileInfo, err error) error {
 	if !glob.Glob("*.rccd", strings.ToLower(filepath.Base(path))) {
 		return nil
 	}
@@ -457,14 +437,20 @@ func (c *Client) visit(path string, f os.FileInfo, err error) error {
 	if rccd, err := rccd.Open(path); err != nil {
 		return err
 	} else {
-		c.rccds = append(c.rccds, rccd)
+		client.rccds = append(client.rccds, rccd)
 	}
 
 	return nil
 }
 
-func (c *Client) ListenAndServe() {
+func (client *Client) ListenAndServe() {
 	log.Info("Starting client....")
+
+	if t, err := template.New("index.html").ParseFiles("./static/index.html"); err != nil {
+		panic(err)
+	} else {
+		client.template = t
+	}
 
 	keytalkPath := ".keytalk"
 	if usr, err := user.Current(); err != nil {
@@ -479,7 +465,7 @@ func (c *Client) ListenAndServe() {
 	}
 
 	// todo(nl5887): first generate personal ca if not exists in cache folder
-	ca, err := c.GenerateNewCA()
+	ca, err := client.GenerateNewCA()
 	if err != nil {
 		panic(err)
 	}
@@ -488,11 +474,11 @@ func (c *Client) ListenAndServe() {
 	// todo(nl5887): save generated certificates to cache folder
 	// todo(nl5887): arguments for starting, -c for config
 
-	proxy := goproxy.NewProxyHttpServer()
-
-	if err := filepath.Walk(keytalkPath, c.visit); err != nil {
+	if err := filepath.Walk(keytalkPath, client.visit); err != nil {
 		panic(err)
 	}
+
+	proxy := goproxy.NewProxyHttpServer()
 
 	proxy.OnRequest().
 		HandleConnect(goproxy.FuncHttpsHandler(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
@@ -502,7 +488,7 @@ func (c *Client) ListenAndServe() {
 			if h, _, err := net.SplitHostPort(host); err != nil {
 				return goproxy.OkConnect, host
 			} else {
-				for _, rccd := range c.rccds {
+				for _, rccd := range client.rccds {
 					for _, provider := range rccd.Providers {
 						for _, service := range provider.Services {
 							if u, err := url.Parse(service.Uri); err != nil {
@@ -511,15 +497,16 @@ func (c *Client) ListenAndServe() {
 								continue
 							}
 
-							// provider is name of kt server
-							// service name of service
-							if loggedIn {
-								ctx.RoundTripper = &RoundTripper2{}
+							if loggedIn /* for this provider */ {
+								ctx.RoundTripper = &RoundTripper2{
+									client: client,
+								}
 							} else {
 								ctx.RoundTripper = &RoundTripper{
 									rccd:     rccd,
 									provider: &provider,
 									service:  &service,
+									client:   client,
 								}
 							}
 
@@ -545,159 +532,4 @@ func (c *Client) ListenAndServe() {
 	if err := http.ListenAndServe(*addr, lh); err != nil {
 		panic(err)
 	}
-
-}
-
-func (c *Client) listenAndServe() {
-	for {
-		conn, err := c.listener.Accept()
-		if err != nil {
-			log.Error("server: accept: %s", err)
-			break
-		}
-
-		go c.handle(conn)
-	}
-}
-
-func (c *Client) handle(conn net.Conn) {
-	if err := recover(); err != nil {
-		trace := make([]byte, 1024)
-		count := runtime.Stack(trace, true)
-		log.Error("Error: %s", err)
-		log.Debug("Stack of %d bytes: %s\n", count, trace)
-		return
-	}
-
-	defer conn.Close()
-
-	tlscon, ok := conn.(*openssl.Conn)
-	if !ok {
-		log.Error("Could not type assert tls.Conn")
-		return
-	}
-
-	defer tlscon.Close()
-
-	reader := bufio.NewReader(tlscon)
-
-	var req *http.Request
-	var err error
-
-	defer func() {
-		if err == nil {
-			return
-		} else if err == io.EOF {
-			return
-		}
-
-		log.Error("Error: ", err.Error())
-
-		resp := &http.Response{
-			Header:     make(http.Header),
-			Request:    req,
-			StatusCode: http.StatusUnauthorized,
-		}
-
-		resp.Header.Set("Server", "Keytalk Authentication Proxy")
-
-		body := fmt.Sprintf("Keytalk proxy error: %s", err.Error())
-
-		r := strings.NewReader(body)
-		resp.Body = ioutil.NopCloser(r)
-
-		resp.Write(tlscon)
-	}()
-
-	if err := tlscon.Handshake(); err == io.EOF {
-		err = nil
-		return
-	} else if err != nil {
-		log.Error("server: handshake failed: %s. Continuing anonymously.\n", err.Error())
-		return
-	}
-
-	cert, err := tlscon.PeerCertificate()
-	if err != nil {
-
-	}
-
-	req, err = http.ReadRequest(reader)
-	if err != nil {
-		return
-	}
-
-	host, _, err := net.SplitHostPort(req.Host)
-	if err != nil {
-		host = req.Host
-	}
-
-	_ = host
-
-	commonName := ""
-	if cert == nil {
-		// err
-		return
-	}
-
-	subject, err := cert.GetSubjectName()
-	if err != nil {
-		log.Error(err.Error())
-		return
-	}
-
-	if s, ok := subject.GetEntry(openssl.NID_commonName); ok {
-		commonName = s
-	}
-
-	_ = commonName
-	return
-	/*
-		for {
-			req.Host = backend.Host(host)
-
-			req.URL = &url.URL{
-				Scheme:   "https",
-				Host:     req.Host,
-				Path:     req.URL.Path,
-				RawQuery: req.URL.RawQuery,
-				Fragment: req.URL.Fragment,
-			}
-
-			req.Header.Del("Accept-Encoding")
-
-			dump, _ := httputil.DumpRequest(req, false)
-			log.Debug("Request: %s", string(dump))
-
-			var resp *http.Response
-			if resp, err = t.RoundTrip(req); err != nil {
-				return
-			}
-
-			switch resp.StatusCode {
-			case 301:
-				// TODO: rewrite location urls
-			case 403:
-				// TODO: try to sign in again
-			}
-
-			resp.Body = NewChangeStream(resp.Body)
-
-			dump, _ = httputil.DumpResponse(resp, false)
-			if err = resp.Write(tlscon); err != nil {
-				return
-			}
-
-			// TODO: add apache compatible format
-			log.Info("%s %s %s %d %s %s", req.Host, req.URL.String(), req.Header.Get("Content-Type"), resp.StatusCode, commonName, req.Header.Get("Referer"))
-
-			// for keep alive, next request
-			req, err = http.ReadRequest(reader)
-			if err == io.EOF {
-				return
-			} else if err != nil {
-				return
-			}
-		}
-	*/
 }
