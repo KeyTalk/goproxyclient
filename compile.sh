c11c66cb26e26896044996419bb9559742965565
Remco <remco@dutchcoders.io>
2017-02-07 17:37:48 +0100

    added compile.sh

---
+++ compile.sh
@@ -0,0 +1,4 @@
+go generate bindata/bindata.go
+CGO_CFLAGS="-I/Users/remco/Projects/keytalk/keytalk-client/src/github.com/KeyTalk/goproxyclient/openssl-1.0.2j/include/" CGO_LDFLAGS="-L/Users/remco/Projects/keytalk/keytalk-client/src/github.com/KeyTalk/goproxyclient/openssl-1.0.2j/" go build -o bin/keytalk-proxy -ldflags=-s
+codesign -f -s "Mac Developer: Paul van Vliet (Y8EZZKZSJP)" --entitlements "./keytalk-proxy.entitlements" "bin/keytalk-proxy" # -D "bin/keytalk-proxy.signed"
+
